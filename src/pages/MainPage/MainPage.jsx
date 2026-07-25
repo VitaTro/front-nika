6a744fc5acf7bc43e59136f667bd5b5124e126cc
VitaTro <144832571+VitaTro@users.noreply.github.com>
2026-07-25 15:45:57 +0200

    + new baner

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -7,14 +7,14 @@ import BannerCarousel from "../../components/BannerCarousel/BannerCarousel";
 import Loader from "../../components/Loader";
 import PopularCarousel from "../../components/PopularCarousel/PopularCarousel";
 import ReviewsCarousel from "../../components/ReviewsCarousel/ReviewsCarousel";
-import ProductsPage from "../ProductsPage/ProductsPage";
-
 import { reviews } from "../../data/reviews";
 import { fetchPublicMain } from "../../redux/main/mainOperations";
 import { fetchUserMain } from "../../redux/user/userOperations";
 import { selectWishlistProducts } from "../../redux/wishlist/selectorsWishlist";
+import ProductsPage from "../ProductsPage/ProductsPage";
+import Girl from "./xupingGirl.png";
 
-import { HomeSeoText, HomeTitle } from "./MainPage.styled";
+import { HomeTitle } from "./MainPage.styled";
 
 const MainPage = () => {
   const isUserAuthenticated = useSelector((state) => state.userAuth.isLoggedIn);
@@ -44,16 +44,49 @@ const MainPage = () => {
       </Helmet>
 
       <BannerCarousel />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: "40px",
+          padding: "40px 20px",
+          maxWidth: "1200px",
+          margin: "0 auto",
+        }}
+      >
+        <img
+          src={Girl}
+          alt="Xuping Jewelry model"
+          style={{
+            width: "100%",
+            maxWidth: "380px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+            flexShrink: 0,
+          }}
+        />
 
-      <HomeTitle>{t("meta.home.description")}</HomeTitle>
+        <div
+          style={{
+            maxWidth: "600px",
+            color: "#5a4a3f",
+            lineHeight: "1.6",
+            textAlign: "left",
+          }}
+        >
+          <HomeTitle>{t("meta.home.description1")}</HomeTitle>
+
+          <p>{t("meta.home.description2")}</p>
+          <p>{t("meta.home.description3")}</p>
+          <p>{t("meta.home.description4")}</p>
+        </div>
+      </div>
 
       <ProductsPage isUserAuthenticated={isUserAuthenticated} />
       <PopularCarousel />
       <ReviewsCarousel reviews={reviews} />
-
-      <HomeSeoText>
-        <p>{/* тут буде текст, який я напишу для тебе */}</p>
-      </HomeSeoText>
     </>
   );
 };
